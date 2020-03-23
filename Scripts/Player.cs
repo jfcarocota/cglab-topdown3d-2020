af132ddac01bb522b89bf273794b4172ce9bd8bb
jfcarocota <jfcarocota@gmail.com>
2020-03-23 11:05:06 -0700

    publish and movement

---
+++ Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   [SerializeField]
+   float moveSpeed;
+
+    void Update() 
+    {
+        transform.Translate(Axis * moveSpeed * Time.deltaTime);
+    }
+
+    Vector3 Axis
+    {
+        get => new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+}
